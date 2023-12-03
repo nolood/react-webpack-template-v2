62322351bf132162fb672a59437e620687f801af
nolood <a9514548799@gmail.com>
2023-12-03 12:00:48 +0500

    commit test

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -2,7 +2,7 @@ import styles from "./button.module.scss";
 import { type FC } from "react";
 
 const Button: FC = () => {
-  return <div className={styles.button}>dasdasda</div>;
+  return <div className={styles.button}>button module</div>;
 };
 
 export default Button;
